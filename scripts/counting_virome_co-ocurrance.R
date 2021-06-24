0049532a3231f931bf7a57d5a2c63d2e8311ff5d
Ricardo I Alcala <ricardoi@Ricardos-MacBook-Pro.local>
2021-06-24 11:58:53 -0400

    virome network metrics R script

--- scripts/counting_virome_co-ocurrance.R
+++ scripts/counting_virome_co-ocurrance.R
@@ -31,9 +31,9 @@ ggplot(vir, aes(cluster))+
 # c("red", "yellow", "purple", "blue", "orange", "pink","black")
 ##############
 
-
-vir <- ddply(virome, .(IDs, Acronym), summarise, cov = mean(Coverage))
-vir <-  tidyr::spread(vir, IDs, cov, drop=T, fill = 0)
+# CREATING A NETWORK BY CLUSTERS
+vir <- ddply(virome, .(cluster, Acronym), summarise, cov = mean(Coverage))
+vir <-  tidyr::spread(vir, cluster, cov, drop=T, fill = 0)
 rownames(vir) <- vir$Acronym
 vir <- vir[-c(1)]
 n=length(vir)
@@ -43,62 +43,47 @@ paste("MESSAGE:: The lenght of the sample locations is ", length(vir), " and the
 
 virgraph <- graph_from_incidence_matrix(vir)
 
-plot(virgraph)
+plot(virgraph, edge.arrow.size=1, vertex.shape=shapes, vertex.size=V(virgraph)$width , 
+     vertex.label.cex=1, vertex.label.color='black', vertex.frame.color="gray", 
+     vertex.frame.color="gold",   edge.curved=F,  layout=layout_with_dh(virgraph, 
+                                                  maxiter=100, cool.fact=0.75)) 
 
-
-# virome0 <- as_tibble(ddply(virome, .(IDs, Acronym, cluster), summarise, Cov= Coverage/Coverage))
-# table(virome0$IDs)
-# table(virome$IDs, virome$cluster)
-
-#virome0 <- as_tibble(ddply(kvina, .(IDs, Acronym, cluster), summarise, Cov= RPKM_mean/RPKM_mean))
-#virome0
-
-#virome <-  as_tibble(ddply(virome0, .(IDs, Acronym, cluster), summarise, count= (Cov)))
-#virome
-
-ggplot(virome0, aes(cluster))+
-        geom_bar(aes(fill=Acronym))
-
-#virome0=viromeReads[viromeReads$Coverage > 1,]
-#IDs.mastre <- virome0$IDs[virome0$Acronym == "SPSMV-1"] 
-#mastre.0 <- virome0[which(virome0$IDs %in% IDs.mastre), ]
-# table(mastre.0$Acronym)
 # plotting incidence
 # pdf("SSA-SPV_all_incidence.pdf", width = 15, height = 30)
 # par(mfrow=c(4,2), mar=c(8,8,8,8))
 # All
-plot(sort(table(virome$IDs), decreasing = T), las= 2, cex.axis = 0.5,lwd=5,
-     ylim=c(0,1350), ylab = "Virus Incidence", main="SSA-SPV virus incidence")
-abline(h=50, col = "red")
-# cluster 1
-plot(sort(table(virome0[which(virome0$cluster==1),]$Species), decreasing = T), las= 2, cex.axis = 0.5,lwd=5,
-     ylim=c(0,228), ylab = "Virus Incidence", main="SSA-SPV virus incidence")
-abline(h=50, col = "red")
-# cluster 2
-plot(sort(table(virome0[which(virome0$cluster==2),]$Species), decreasing = T), las= 2, cex.axis = 0.5,lwd=5,
-     ylim=c(0,36), ylab = "Virus Incidence", main="SSA-SPV virus incidence")
-abline(h=50, col = "red")
-# cluster 3
-plot(sort(table(virome0[which(virome0$cluster==3),]$Species), decreasing = T), las= 2, cex.axis = 0.5,lwd=5,
-     ylim=c(0,171), ylab = "Virus Incidence", main="SSA-SPV virus incidence")
-abline(h=50, col = "red")
-# cluster 4
-plot(sort(table(virome0[which(virome0$cluster==4),]$Species), decreasing = T), las= 2, cex.axis = 0.5,lwd=5,
-     ylim=c(0,262), ylab = "Virus Incidence", main="SSA-SPV virus incidence")
-abline(h=50, col = "red")
-# cluster 5
-plot(sort(table(virome0[which(virome0$cluster==5),]$Species), decreasing = T), las= 2, cex.axis = 0.5,lwd=5,
-     ylim=c(0,151), ylab = "Virus Incidence", main="SSA-SPV virus incidence")
-abline(h=50, col = "red")
-# cluster 6
-plot(sort(table(virome0[which(virome0$cluster==6),]$Species), decreasing = T), las= 2, cex.axis = 0.5,lwd=5,
-     ylim=c(0,261), ylab = "Virus Incidence", main="SSA-SPV virus incidence")
-abline(h=50, col = "red")
-# cluster 7
-plot(sort(table(virome0[which(virome0$cluster==7),]$Species), decreasing = T), las= 2, cex.axis = 0.5,lwd=5,
-     ylim=c(0,500), ylab = "Virus Incidence", main="SSA-SPV virus incidence")
-abline(h=50, col = "red")
-# dev.off()
+# plot(sort(table(virome$IDs), decreasing = T), las= 2, cex.axis = 0.5,lwd=5,
+#      ylim=c(0,1350), ylab = "Virus Incidence", main="SSA-SPV virus incidence")
+# abline(h=50, col = "red")
+# # cluster 1
+# plot(sort(table(virome0[which(virome0$cluster==1),]$Species), decreasing = T), las= 2, cex.axis = 0.5,lwd=5,
+#      ylim=c(0,228), ylab = "Virus Incidence", main="SSA-SPV virus incidence")
+# abline(h=50, col = "red")
+# # cluster 2
+# plot(sort(table(virome0[which(virome0$cluster==2),]$Species), decreasing = T), las= 2, cex.axis = 0.5,lwd=5,
+#      ylim=c(0,36), ylab = "Virus Incidence", main="SSA-SPV virus incidence")
+# abline(h=50, col = "red")
+# # cluster 3
+# plot(sort(table(virome0[which(virome0$cluster==3),]$Species), decreasing = T), las= 2, cex.axis = 0.5,lwd=5,
+#      ylim=c(0,171), ylab = "Virus Incidence", main="SSA-SPV virus incidence")
+# abline(h=50, col = "red")
+# # cluster 4
+# plot(sort(table(virome0[which(virome0$cluster==4),]$Species), decreasing = T), las= 2, cex.axis = 0.5,lwd=5,
+#      ylim=c(0,262), ylab = "Virus Incidence", main="SSA-SPV virus incidence")
+# abline(h=50, col = "red")
+# # cluster 5
+# plot(sort(table(virome0[which(virome0$cluster==5),]$Species), decreasing = T), las= 2, cex.axis = 0.5,lwd=5,
+#      ylim=c(0,151), ylab = "Virus Incidence", main="SSA-SPV virus incidence")
+# abline(h=50, col = "red")
+# # cluster 6
+# plot(sort(table(virome0[which(virome0$cluster==6),]$Species), decreasing = T), las= 2, cex.axis = 0.5,lwd=5,
+#      ylim=c(0,261), ylab = "Virus Incidence", main="SSA-SPV virus incidence")
+# abline(h=50, col = "red")
+# # cluster 7
+# plot(sort(table(virome0[which(virome0$cluster==7),]$Species), decreasing = T), las= 2, cex.axis = 0.5,lwd=5,
+#      ylim=c(0,500), ylab = "Virus Incidence", main="SSA-SPV virus incidence")
+# abline(h=50, col = "red")
+# # dev.off()
 
 bin = table(virome0$IDs, virome0$Acronym)
 
---
+++ scripts/virome_networks_metric.R
@@ -0,0 +1,12 @@
+#'@title:  "Virome Networks Metric"
+#'@author: "R Alcala"
+#'@date:   "06/24/2021"
+#'@output: "Plots metrics vs regions"
+#---------------------------- species co-occurrence --------------------------- 
+# NECESITO CARGAR EL NUEVO DATOS 0-KCLUSTER...
+#----------------- Install & load libraries ------------------
+library(tidyverse)
+# library(dplyr)
+# library(plyr)
+# library(igraph )
+#### NOTE
