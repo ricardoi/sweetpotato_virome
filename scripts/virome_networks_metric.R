773c51a28a43aebc554e67b7b6b8da2450b24a85
ricardoi <ralcala@ufl.edu>
2021-06-30 12:35:19 -0400

    node metrics Tufte style

--- scripts/virome_networks_metric.R
+++ scripts/virome_networks_metric.R
@@ -2,11 +2,249 @@
 #'@author: "R Alcala"
 #'@date:   "06/24/2021"
 #'@output: "Plots metrics vs regions"
-#---------------------------- species co-occurrence --------------------------- 
-# NECESITO CARGAR EL NUEVO DATOS 0-KCLUSTER...
+#---------------------------- virome network metrics  --------------------------- 
+setwd("~/git_local/sweetpotato_virome/data/netork_metrics/")
+
 #----------------- Install & load libraries ------------------
 library(tidyverse)
-# library(dplyr)
-# library(plyr)
+#library(dplyr)
+#library(plyr)
 # library(igraph )
 #### NOTE
+
+#------- Loading files
+files = list.files(pattern="*network-metrics.csv")
+#
+for (i in 1:length(files)){
+  x <- read.csv(files[i], as.is=T)
+  # x$IDs <- rep(names$names[i], nrow(x))
+  ls[[i]] = x
+}
+ls[[1]]
+
+nmts <- cbind(ls[[1]], ls[[2]], ls[[3]], ls[[4]], ls[[5]], ls[[6]], ls[[7]])
+colnames(nmts) <- c(1:7)
+nmts <- as.data.frame(t(nmts))
+#####
+names(nmts)
+# Figure
+par(mfrow=c(4,2), mar=c(3,3,3,3))
+plot(nmts$nestedness, ylab = "Nestedness", xlab="Sweetpotato groups", 
+     main="SSA-SPV virus nestedness")
+plot(nmts$`weighted nestedness`, ylab = "weighted nestedness", xlab="Sweetpotato groups", 
+     main="SSA-SPV virus weighted nestedness")
+plot(nmts$NODF, ylab = "NODF", xlab="Sweetpotato groups",
+     main="SSA-SPV virus NODF")
+plot(nmts$`connectance`, ylab = "connectance", xlab="Sweetpotato groups",
+     main="SSA-SPV virus connectance")
+plot(nmts$`weighted connectance`, ylab = "weighted connectance", xlab="Sweetpotato groups",
+     main="SSA-SPV virus weighted connectance")
+plot(nmts$`Fisher alpha`, ylab = "Fisher alpha", xlab="Sweetpotato groups",
+     main="SSA-SPV virus Fisher alpha")
+plot(nmts$`Shannon diversity`, ylab = "Shannon diversity", xlab="Sweetpotato groups",
+     main="SSA-SPV virus Shannon diversity")
+plot(nmts$number.of.species.LL ~ nmts$number.of.species.HL, ylab = "No. of species (host)", xlab="No. of species (virus",
+     main="SSA-SPV host-virus species")
+# dev.off()
+
+#------ Node metrics HL
+files2 = list.files(pattern="*metrics_hl.csv")
+names <- strsplit(files2, split = "_")
+#
+for (i in 1:length(files2)){
+  x <- read.csv(files2[i], as.is=T)
+  x$IDs <- rep(names[[i]][1], nrow(x))
+  ls[[i]] = x
+  colnames(ls[[i]]) <- colnames(ls[[1]])
+}
+
+nohl <- rbind(ls[[1]], ls[[2]], ls[[3]], ls[[4]], ls[[5]], ls[[6]], ls[[7]])
+class(nohl)
+as_tibble(nohl)
+
+ggplot(nohl, aes(IDs, degree))+ 
+  geom_count(shape = 1, show.legend=T) +
+  labs(subtitle="Higher level SSA-SPV", 
+       y="Node degree", 
+       x="Sweetpotato region", 
+       title="Count Points")
+#-
+library(ggthemes)
+# plot
+#----- degree
+ggplot(nohl, aes(IDs, degree))+
+  geom_tufteboxplot(voffset = 0.0, hoffset = 0, median.type = "line") + 
+  # geom_point()+
+  stat_summary(fun = "mean", geom = "point", col = "black", size = 3, shape = 1)+
+  theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+  # theme_tufte() +
+  labs(title="Tufte Styled Boxplot", 
+       subtitle="Lower level SSA-SPVe",
+       caption="Source: mpg",
+       x="Node degreee",
+       y="Sweetpotato regions")
+
+#----- species.strength
+ggplot(nohl, aes(IDs, species.strength))+
+  geom_tufteboxplot(voffset = 0.0, hoffset = 0, median.type = "line") + 
+  # geom_point()+
+  stat_summary(fun = "mean", geom = "point", col = "black", size = 3, shape = 1)+
+  theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+  # theme_tufte() +
+  labs(title="Tufte Styled Boxplot", 
+       subtitle="Lower level SSA-SPVe",
+       caption="Source: mpg",
+       x="Node degreee",
+       y="Sweetpotato regions")
+
+#----- species.specificity.index
+ggplot(nohl, aes(IDs, species.specificity.index))+
+  geom_tufteboxplot(voffset = 0.0, hoffset = 0, median.type = "line") + 
+  # geom_point()+
+  stat_summary(fun = "mean", geom = "point", col = "black", size = 3, shape = 1)+
+  theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+  # theme_tufte() +
+  labs(title="Tufte Styled Boxplot", 
+       subtitle="Lower level SSA-SPVe",
+       caption="Source: mpg",
+       x="Node degreee",
+       y="Sweetpotato regions")
+
+#----- weighted.betweenness
+ggplot(nohl, aes(IDs, weighted.betweenness))+
+  geom_tufteboxplot(voffset = 0.0, hoffset = 0, median.type = "line") + 
+  # geom_point()+
+  stat_summary(fun = "mean", geom = "point", col = "black", size = 3, shape = 1)+
+  theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+  # theme_tufte() +
+  labs(title="Tufte Styled Boxplot", 
+       subtitle="Lower level SSA-SPVe",
+       caption="Source: mpg",
+       x="Node degreee",
+       y="Sweetpotato regions")
+
+#----- weighted.closeness
+ggplot(nohl, aes(IDs, weighted.closeness))+
+  geom_tufteboxplot(voffset = 0.0, hoffset = 0, median.type = "line") + 
+  # geom_point()+
+  stat_summary(fun = "mean", geom = "point", col = "black", size = 3, shape = 1)+
+  theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+  # theme_tufte() +
+  labs(title="Tufte Styled Boxplot", 
+       subtitle="Lower level SSA-SPVe",
+       caption="Source: mpg",
+       x="Node degreee",
+       y="Sweetpotato regions")
+
+#----- Fisher.alpha
+ggplot(nohl, aes(IDs, Fisher.alpha))+
+  geom_tufteboxplot(voffset = 0.0, hoffset = 0, median.type = "line") + 
+  # geom_point()+
+  stat_summary(fun = "mean", geom = "point", col = "black", size = 3, shape = 1)+
+  theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+  # theme_tufte() +
+  labs(title="Tufte Styled Boxplot", 
+       subtitle="Lower level SSA-SPVe",
+       caption="Source: mpg",
+       x="Node degreee",
+       y="Sweetpotato regions")
+
+#----- Fisher.alpha
+ggplot(nohl, aes(IDs, Fisher.alpha))+
+  geom_tufteboxplot(voffset = 0.0, hoffset = 0, median.type = "line") + 
+  # geom_point()+
+  stat_summary(fun = "mean", geom = "point", col = "black", size = 3, shape = 1)+
+  theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+  # theme_tufte() +
+  labs(title="Tufte Styled Boxplot", 
+       subtitle="Lower level SSA-SPVe",
+       caption="Source: mpg",
+       x="Node degreee",
+       y="Sweetpotato regions")
+
+#----- Fisher.alpha
+ggplot(nohl, aes(IDs, Fisher.alpha))+
+  geom_tufteboxplot(voffset = 0.0, hoffset = 0, median.type = "line") + 
+  # geom_point()+
+  stat_summary(fun = "mean", geom = "point", col = "black", size = 3, shape = 1)+
+  theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+  # theme_tufte() +
+  labs(title="Tufte Styled Boxplot", 
+       subtitle="Lower level SSA-SPVe",
+       caption="Source: mpg",
+       x="Node degreee",
+       y="Sweetpotato regions")
+
+#----- Fisher.alpha
+ggplot(nohl, aes(IDs, Fisher.alpha))+
+  geom_tufteboxplot(voffset = 0.0, hoffset = 0, median.type = "line") + 
+  # geom_point()+
+  stat_summary(fun = "mean", geom = "point", col = "black", size = 3, shape = 1)+
+  theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+  # theme_tufte() +
+  labs(title="Tufte Styled Boxplot", 
+       subtitle="Lower level SSA-SPVe",
+       caption="Source: mpg",
+       x="Node degreee",
+       y="Sweetpotato regions")
+
+
+
+
+#------ Node metrics LL
+files3 = list.files(pattern="*metrics_ll.csv")
+names <- strsplit(files3, split = "_")
+#
+for (i in 1:length(files3)){
+  x <- read.csv(files3[i], as.is=T)
+  x$IDs <- rep(names[[i]][1], nrow(x))
+  ls[[i]] = x
+  colnames(ls[[i]]) <- colnames(ls[[1]])
+}
+
+noll <- rbind(ls[[1]], ls[[2]], ls[[3]], ls[[4]], ls[[5]], ls[[6]], ls[[7]])
+as_tibble(noll)
+
+#===
+ggplot(noll, aes(IDs, degree))+ 
+  geom_count(aes(group = IDs),shape = 1, show.legend=T) +
+  # scale_size_area()+
+  labs(subtitle="Higher level SSA-SPV", 
+       y="Node degree", 
+       x="Sweetpotato region", 
+       title="Count Points")
+
+
+#------
+library(ggthemes)
+# plot
+ggplot(noll, aes(IDs, degree))+
+  geom_tufteboxplot(voffset = 0.0, hoffset = 0, median.type = "line") + 
+  # geom_point()+
+  stat_summary(fun = "mean", geom = "point", col = "black", size = 3, shape = 1)+
+  theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+  # theme_tufte() +
+  labs(title="Tufte Styled Boxplot", 
+       subtitle="Lower level SSA-SPVe",
+       caption="Source: mpg",
+       x="Node degreee",
+       y="Sweetpotato regions")
+
+
+
+
+
+
+
+
+
+
+#----------------
+
+hl1 <- read.csv("k-cluster1_node-metrics_hl.csv", as.is=T)
+hl1
+
+ll1 <- read.csv("k-cluster1_node-metrics_ll.csv", as.is=T) 
+ll1
+mean(ll1$degree)
+max(ll1$degree)
